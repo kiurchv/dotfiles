413ecffed21004f29e79c96ccc466d2da926a38f
Myroslav Kiurchev <kiurchv@gmail.com>
2023-06-14 23:00:39 +0300

    Setup Android SDK envvars

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -38,3 +38,10 @@ set SPACEFISH_CHAR_SYMBOL '❯'
 
 # initialize asdf
 source /usr/local/opt/asdf/asdf.fish
+
+# Android SDK
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x PATH $PATH $ANDROID_HOME/emulator
+set -x PATH $PATH $ANDROID_HOME/tools
+set -x PATH $PATH $ANDROID_HOME/tools/bin
+set -x PATH $PATH $ANDROID_HOME/platform-tools
